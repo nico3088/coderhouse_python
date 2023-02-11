5c9f0454829e364e712ae355bbd3ccb865e20cee
--unset <nicogarnero1@outlook.com>
2023-02-11 16:31:52 -0300

    correcion de los comentarios

--- unidad1.py
+++ unidad1.py
@@ -1,6 +1,9 @@
-# En nuestro trabajo, nos solicitan desarrollar un programa que permita calcular el promedio final de los equipos de futbol en un torneo. Para ello, debemos considerar tres aspectos:
-# jugaron 20 partidos durante el torneo, los partidos poseen diferente puntaje según el resultado, los partidos ganados 3 puntos, partido empatado 1 punto, partido perdido 0 puntos, el promedio final resulta de la cantidad de puntos totales divididos la cantidad de partidos
-# La cantidad de partidos que debemos considerar a un equipo para el ejemplo se detallan a continuación:
-# partidos_ganados 8 partido_empatados 7 partido_perdidos 5
-# Importante: Cada una de las cantidades de partidos ganados, empatados o perdidos debe solicitarse al usuario utilizando la función input().
-# Deslogueo de github
+""" 
+En nuestro trabajo, nos solicitan desarrollar un programa que permita calcular el promedio final de los equipos de futbol en un torneo. Para ello, debemos considerar tres aspectos:
+jugaron 20 partidos durante el torneo, los partidos poseen diferente puntaje según el resultado, los partidos ganados 3 puntos, partido empatado 1 punto, partido perdido 0 puntos, el promedio final resulta de la cantidad de puntos totales divididos la cantidad de partidos
+La cantidad de partidos que debemos considerar a un equipo para el ejemplo se detallan a continuación:
+partidos_ganados 8 partido_empatados 7 partido_perdidos 5
+Importante: Cada una de las cantidades de partidos ganados, empatados o perdidos debe solicitarse al usuario utilizando la función input().
+Deslogueo de github 
+"""
+
