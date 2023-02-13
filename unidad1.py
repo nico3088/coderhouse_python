59ccba23749a01862adbd5b1822404780cd83795
--unset <nicogarnero1@outlook.com>
2023-02-13 19:55:07 -0300

    Segundo ejercicio unidad 1

--- unidad1.py
+++ unidad1.py
@@ -23,3 +23,15 @@ puntaje_total =(puntaje_partidos_ganados * partidos_ganados) + (puntaje_partidos
 promedio_equipo = puntaje_total / cantidad_de_partidos
 print (promedio_equipo)
 
+"""Segundo_desafio_unidad_1
+Dadas cuatro variables con diferentes textos (de forma individual), genera una nueva variable con el siguiente contenido:
+
+Objetivo: “Python es un lenguaje de programación versátil” """
+
+cadena_1 = "versátil"
+cadena_2 = "Python"
+cadena_3 = "es un lenguaje"
+cadena_4 = "de programación"
+
+print (cadena_2, cadena_3, cadena_4, cadena_1)
+
